85236a694274a5e49544d5b9d003bea46f09d73c
yp0975eu(Branden) <yp0975eu@users.noreply.github.com>
2016-11-08 15:17:18 -0600

    Add welcome string, signing button to sign-in layout

--- app/src/main/java/com/example/me5013zu/scavengerhunt/SignInActivity.java
+++ app/src/main/java/com/example/me5013zu/scavengerhunt/SignInActivity.java
@@ -9,5 +9,6 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
     }
 }
